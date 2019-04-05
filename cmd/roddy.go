0497d90b682c64da57416f348a9798a82aee9a42
Gal Tashma <galtashma@gmail.com>
2019-04-06 01:57:04 +0800

    Added static index

--- cmd/roddy.go
+++ cmd/roddy.go
@@ -8,9 +8,9 @@ package main
 
 import (
 	"flag"
-	"html/template"
 	"log"
 	"net/http"
+        "io/ioutil"
 
 	"github.com/gorilla/websocket"
 )
@@ -47,93 +47,25 @@ func echo(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+var index []byte
+
 func home(w http.ResponseWriter, r *http.Request) {
-	homeTemplate.Execute(w, "ws://"+r.Host+"/echo")
+    w.Header().Set("Content-Type", "text/html")
+    w.Write(index)
 }
 
 func main() {
 	flag.Parse()
 	log.SetFlags(0)
 	http.HandleFunc("/echo", echo)
-	http.HandleFunc("/", home)
+        http.HandleFunc("/", home)
+
+        index, _ = ioutil.ReadFile("static/index.html")
+
+        fs := http.FileServer(http.Dir("static"))
+        http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+
 	log.Fatal(http.ListenAndServe(*addr, nil))
 }
 
-var homeTemplate = template.Must(template.New("").Parse(`
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8">
-<script>  
-window.addEventListener("load", function(evt) {
-
-    var output = document.getElementById("output");
-    var input = document.getElementById("input");
-    var ws;
-
-    var print = function(message) {
-        var d = document.createElement("div");
-        d.innerHTML = message;
-        output.appendChild(d);
-    };
-
-    document.getElementById("open").onclick = function(evt) {
-        if (ws) {
-            return false;
-        }
-        ws = new WebSocket("{{.}}");
-        ws.onopen = function(evt) {
-            print("OPEN");
-        }
-        ws.onclose = function(evt) {
-            print("CLOSE");
-            ws = null;
-        }
-        ws.onmessage = function(evt) {
-            print("RESPONSE: " + evt.data);
-        }
-        ws.onerror = function(evt) {
-            print("ERROR: " + evt.data);
-        }
-        return false;
-    };
-
-    document.getElementById("send").onclick = function(evt) {
-        if (!ws) {
-            return false;
-        }
-        print("SEND: " + input.value);
-        ws.send(input.value);
-        return false;
-    };
-
-    document.getElementById("close").onclick = function(evt) {
-        if (!ws) {
-            return false;
-        }
-        ws.close();
-        return false;
-    };
-
-});
-</script>
-</head>
-<body>
-<table>
-<tr><td valign="top" width="50%">
-<p>Click "Open" to create a connection to the server, 
-"Send" to send a message to the server and "Close" to close the connection. 
-You can change the message and send multiple times.
-<p>
-<form>
-<button id="open">Open</button>
-<button id="close">Close</button>
-<p><input id="input" type="text" value="Hello world!">
-<button id="send">Send</button>
-</form>
-</td><td valign="top" width="50%">
-<div id="output"></div>
-</td></tr></table>
-</body>
-</html>
-`))
